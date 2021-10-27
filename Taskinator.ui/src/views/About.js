104ffa81a56a976864e06c829a94148283614238
Lindsey Satterfield <satterfield.lindsey@gmail.com>
2021-10-27 18:41:38 -0500

    working on about view

--- Taskinator.ui/src/views/About.js
+++ Taskinator.ui/src/views/About.js
@@ -3,7 +3,22 @@ import React from 'react';
 function About() {
   return (
     <>
-      <h1>About</h1>
+      <div className="about-us-container">
+        <h1>About Us</h1>
+        <h1>TASKINATOR</h1>
+        <p>
+          TASKINATOR is a robot service here to help users with any at home needs! Do you find yourself out of time to work on tasks such as cleaning, mowing the lawn, scrubbing dishes, dusting, casual murder, or scooping the litter box? If so, give TASKINATOR a try!
+        </p>
+        <p>
+          There is no limit to what TASKINATOR robots can do! No task is too time consuming or filthy for these robots.
+        </p>
+        <p>
+          TASKINATOR can be rented for an amount of days of your choosing. It arrives ready at your house to work and get the tough job done.
+        </p>
+        <p>
+          To get started, please log in select a robot from the services tab.
+        </p>
+      </div>
     </>
   );
 }
