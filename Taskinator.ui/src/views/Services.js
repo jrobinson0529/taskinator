28b85b33dfdff1d837503293fa4efeb410374094
Rob Cole <robertncole1@gmail.com>
2021-10-30 12:23:51 -0400

    added in hero image for services and made dark mode on styling

---
+++ Taskinator.ui/src/views/Services.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ServicesHero from '../components/ServicesHero';
+
+function Services() {
+  return (
+    <>
+    <ServicesHero/>
+    </>
+  );
+}
+
+export default Services;
