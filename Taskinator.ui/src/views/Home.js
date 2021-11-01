723fadc882591d699431a818d09ec6173967e36b
Chie Stroud <urirunrunkoko@gmail.com>
2021-11-01 18:22:30 -0500

    checking rob's code

--- Taskinator.ui/src/views/Home.js
+++ Taskinator.ui/src/views/Home.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import RandomRobotsContainer from '../components/RandomRobotsContainer';
 import HeroImage from '../components/HeroImage';
 import AboutUs from '../components/AboutUs';
 import RobotForm from '../components/forms/RobotForm';
 import getRobotCategories from '../helpers/data/robotCategoryData';
+import ProfileForm from '../components/forms/ProfileForm';
 
-function Home() {
+function Home({ user }) {
   const [robotCategories, setRobotCategories] = useState([]);
   useEffect(() => {
     getRobotCategories().then((response) => setRobotCategories(response));
@@ -18,10 +20,15 @@ function Home() {
       <h2>Check it out!</h2>
       <RandomRobotsContainer />
 
-      <RobotForm robotCategories={robotCategories}/>
+      <RobotForm robotCategories={robotCategories} />
+      <ProfileForm user={user}/>
 
     </>
   );
 }
 
 export default Home;
+
+Home.propTypes = {
+  user: PropTypes.any
+};
