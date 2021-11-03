b1d0ab534cd0e6d9f442cad6bd181425905129f4
Lindsey Satterfield <satterfield.lindsey@gmail.com>
2021-11-02 20:13:24 -0500

    merging

--- Taskinator.ui/src/views/Robot.js
+++ Taskinator.ui/src/views/Robot.js
@@ -16,6 +16,17 @@ export default function Robot() {
       <h1 className="singleRobotTitle">{robot.title}</h1>
       <h2 className="singleRobotPrice">Price: ${robot.price} per day</h2>
       <h3 className="singleRobotDescription">{robot.description}</h3>
+      <div className="form-group singleRobot-dropdown">
+        <label>Day Quantity</label>
+        <select className="form-control" id="singleRobot-select">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </select>
+      </div>
+      <button type="button" className="btn btn-primary btn-lg">Add to Cart</button>
     </div>
   );
 }
