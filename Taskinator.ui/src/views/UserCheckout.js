8ee05a6f37bc956145ba0239a5bbc8de69c82d6f
Chie Stroud <urirunrunkoko@gmail.com>
2021-11-13 09:14:51 -0600

    added class on usercheckout

--- Taskinator.ui/src/views/UserCheckout.js
+++ Taskinator.ui/src/views/UserCheckout.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useParams } from 'react-router-dom';
-import { Card, CardText, CardTitle } from 'reactstrap';
+import {
+  Card, CardBody, CardText, CardTitle
+} from 'reactstrap';
 
 export default function UserCheckout({ user }) {
   const { id } = useParams();
 
   return (
-    <div>
+    <div className="full-height-section">
       <Card className="bg-dark">
-        <CardTitle>Thank you {user.firstName} for your order!</CardTitle>
+        <CardTitle>{user.firstName}, thank you for ordering bots!</CardTitle>
+        <CardBody>Your bots are on their way!</CardBody>
         <CardText>Order ID: { id }</CardText>
       </Card>
     </div>
