cff6b1602e4682767a612e587d9c2c2854b3d99e
Chie Stroud <urirunrunkoko@gmail.com>
2021-11-13 11:25:59 -0600

    checking other branches

--- Taskinator.ui/src/components/OrderHistory.js
+++ Taskinator.ui/src/components/OrderHistory.js
@@ -1,14 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import {
+  Card, CardBody, CardText, CardTitle
+} from 'reactstrap';
 import { getOrderHistory } from '../helpers/data/orderData';
 
 export default function OrderHisotry({ user }) {
+  const [orders, setOrders] = useState([]);
   useEffect(() => {
-    getOrderHistory(user?.id).then((response) => console.warn(response));
+    getOrderHistory(user?.id).then((response) => setOrders(response));
   }, []);
   return (
-    <div>
-      <h1>Order</h1>
+    <div className='order-history-container'>
+      {orders?.map((order) => (
+        <Card className='order-history-card' key={order.id}>
+          <CardTitle>{order.orderDate}</CardTitle>
+          <CardBody>
+            <CardText>Total $ {order.orderTotal}</CardText>
+          </CardBody>
+        </Card>
+      ))}
     </div>
   );
 }
