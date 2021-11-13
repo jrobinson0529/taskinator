3e493a2cc4c5e75003840a434cc85f1d0587ae62
Chie Stroud <urirunrunkoko@gmail.com>
2021-11-13 10:28:56 -0600

    checking branch

--- Taskinator.ui/src/views/UserCheckout.js
+++ Taskinator.ui/src/views/UserCheckout.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useParams } from 'react-router-dom';
 import {
-  Card, CardBody, CardText, CardTitle
+  Card, CardBody, CardTitle
 } from 'reactstrap';
 
 export default function UserCheckout({ user }) {
-  const { id } = useParams();
-
   return (
     <div className="full-height-section">
       <Card className="bg-dark">
         <CardTitle>{user.firstName}, thank you for ordering bots!</CardTitle>
         <CardBody>Your bots are on their way!</CardBody>
-        <CardText>Order ID: { id }</CardText>
       </Card>
     </div>
   );
