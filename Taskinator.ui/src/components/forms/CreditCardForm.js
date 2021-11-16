3dcefa5552cb883b9fb89fc90d0b74de83a1df7a
Rob Cole <robertncole1@gmail.com>
2021-11-15 21:24:26 -0500

    changed payment form and added in credit card npm package

---
+++ Taskinator.ui/src/components/forms/CreditCardForm.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import Cards from 'react-credit-cards';
+import {
+  Col, Container, Form, Input, Row
+} from 'reactstrap';
+
+export default class CreditCardForm extends React.Component {
+  state = {
+    cvc: '',
+    expiry: '',
+    focus: '',
+    name: '',
+    number: '',
+  };
+
+  handleInputFocus = (e) => {
+    this.setState({ focus: e.target.name });
+  }
+
+  handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    this.setState({ [name]: value });
+  }
+
+  render() {
+    return (
+      <div id="PaymentForm">
+        <Container>
+          <Row>
+            <Col>
+              <Cards
+                cvc={this.state.cvc}
+                expiry={this.state.expiry}
+                focused={this.state.focus}
+                name={this.state.name}
+                number={this.state.number}
+              />
+            </Col>
+            <Col>
+              <Form>
+              <Input
+                  type="number"
+                  name="number"
+                  placeholder="Card Number"
+                  onChange={this.handleInputChange}
+                  onFocus={this.handleInputFocus}
+                />
+                <Input
+                  type="text"
+                  name="name"
+                  placeholder="Card Holder Name"
+                  onChange={this.handleInputChange}
+                  onFocus={this.handleInputFocus}
+                />
+                <Input
+                  type="text"
+                  name="expiry"
+                  placeholder="MM / YY"
+                  onChange={this.handleInputChange}
+                  onFocus={this.handleInputFocus}
+                />
+                <Input
+                  type="number"
+                  name="cvc"
+                  placeholder="CVC"
+                  onChange={this.handleInputChange}
+                  onFocus={this.handleInputFocus}
+                />
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
